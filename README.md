ccd6f81e2929dc96b56b1a626cc93e4212212177
Petya Buchkova <petya.buchkova@gmail.com>
2020-05-13 20:13:00 +0200

    Added error boundary component

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Junior to Senior Web Developer Roadmap Udemy course project. This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
 
---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class ErrorBoundry extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    const { hasError } = this.state;
+    return hasError ? (
+      <h1>Oops! This page has an error!</h1>
+    ) : (
+      this.props.children
+    );
+  }
+}
+
+export default ErrorBoundry;
