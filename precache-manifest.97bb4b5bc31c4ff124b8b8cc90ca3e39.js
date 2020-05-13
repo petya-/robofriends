98172f5e18b6d373b9ff987c8abe5eb7d4867e83
Petya Buchkova <petya.buchkova@gmail.com>
2020-05-13 20:15:21 +0200

    Updates

---
+++ precache-manifest.97bb4b5bc31c4ff124b8b8cc90ca3e39.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b351620dc0bfec2fe0358fd2bade622",
+    "url": "/petya-/robofriends/index.html"
+  },
+  {
+    "revision": "d9298428412a7a3d3fa2",
+    "url": "/petya-/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3d2970f19e8c13f6effb",
+    "url": "/petya-/robofriends/static/css/main.853f135c.chunk.css"
+  },
+  {
+    "revision": "d9298428412a7a3d3fa2",
+    "url": "/petya-/robofriends/static/js/2.4e822d6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/petya-/robofriends/static/js/2.4e822d6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d2970f19e8c13f6effb",
+    "url": "/petya-/robofriends/static/js/main.4cdb8652.chunk.js"
+  },
+  {
+    "revision": "e71925d50d6d63af5359",
+    "url": "/petya-/robofriends/static/js/runtime-main.4a3b2580.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/petya-/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/petya-/robofriends/precache-manifest.97bb4b5bc31c4ff124b8b8cc90ca3e39.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/petya-/robofriends/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
